dc7591142349103a40c951763cbbcf82b045d387
Luan Leal <luanlealboni@gmail.com>
2022-06-24 00:23:52 -0300

    Recebendo e postando mensagens do servidor a cada 3 segundos

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
-let nomes = []; 
-/* entrarNaSala()
+ /* let nomes = []; 
+entrarNaSala()
 function entrarNaSala(){
     let nome = prompt("Digite seu lindo nome")
 
-    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",{
+    const usuario = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",{
         name: nome,
     });
 
-    promessa.then(podeEntrar);
-    promessa.catch(escolhaOutronome);
+    usuario.then(podeEntrar);
+    usuario.catch(escolhaOutronome);
 };
 
 function podeEntrar() {
-    console.log("deu certo!")
+    puxaParticipantes();
 }
 
 function escolhaOutronome(erro){
@@ -20,24 +20,76 @@ function escolhaOutronome(erro){
     if(status === 400) {
        entrarNaSala();
     }
-} */
-const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
-promessa.then(chegou)
-console.log(promessa);
+}
+function puxaParticipantes() {
+    const participantes = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    participantes.then(chegou);
+}
 
 function chegou(resposta){
-    console.log("Os dados chegaram");
-    console.log(resposta.data);
     nomes = resposta.data;
     atualizaNomes();
+
 }
 
 function atualizaNomes(){
     const ul = document.querySelector('.conversas');
     for (let i = 0; i < nomes.length; i++) {
-        ul.innerHTML += 
+        ul.innerHTML +=
         `
         <li>${nomes[i].name} entrou na sala</li>
         `
     }
 }
+}*/
+
+let mensagens = [];
+buscarMensagens();
+
+function buscarMensagens() {
+    let from;
+    let to;
+    let text;
+    let type;
+    let time;
+    const mensagens = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages",{
+        from: from,
+        to: to,
+        text: text,
+        type: type,
+        time: time,
+    } );
+
+    mensagens.then(puxaMensagens);
+}
+
+function puxaMensagens(mensagem){
+    mensagens = mensagem.data;
+    insereMensagens();
+}
+
+function insereMensagens(){
+    const ul = document.querySelector('.conversas');
+    for (let i = 0; i < mensagens.length; i++){
+        if(mensagens[i].type === "message") {
+        ul.innerHTML +=
+        ` 
+        <li>${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text} </li>
+        `}
+        else if (mensagens[i].type === "status") {
+            ul.innerHTML +=
+            ` 
+            <li>${mensagens[i].from} entra na sala... </li>
+            `
+        }
+
+        else if (mensagens[i].type === "private_message") {
+            ul.innerHTML +=
+            ` 
+        <li>${mensagens[i].from} reservadamente para ${mensagens[i].to}: ${mensagens[i].text} </li>
+        `
+        }
+    }
+}
+
+setInterval(buscarMensagens, 3000);
